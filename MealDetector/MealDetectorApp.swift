bff63be6fc5bf8c69a1dab4a65828344013cfbf4
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2025-11-10 16:31:06 +0100

    Initial Commit

---
+++ MealDetector/MealDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDetectorApp.swift
+//  MealDetector
+//
+//  Created by Viranaiken Jessy on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MealDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
